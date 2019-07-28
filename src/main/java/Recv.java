5b350b90aed35269939b6a0f79a8780f2540bd5d
zhengjia <zhengjia@192.168.0.102>
2019-07-28 12:01:36 +0800

    a simple Hello World MQ

---
+++ src/main/java/Recv.java
@@ -0,0 +1,28 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Recv {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception{
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection =factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waitting for messages. To exit press Ctrl+C");
+
+        // 这一句其实是实现了DeliverCallback的一个接口handle
+        DeliverCallback deliverCallback = (consumerTag, delivery) ->{
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message +"'");
+        
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+
+
+    }
+}
